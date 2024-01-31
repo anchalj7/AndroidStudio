02797de9afe747d1a1417cbdbb7cd93a165b3345
anchalj7 <149676596+anchaljaiswal0301@users.noreply.github.com>
2024-01-31 18:30:47 +0530

    Assignment

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,15 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         final EditText editTextMeters = findViewById(R.id.editTextMeters);
         final TextView textviewResult = findViewById(R.id.textviewResult);
-        Button convertButton =findViewById (R.id.convertButton);
+        Button convertButton = findViewById(R.id.convertButton);
 
-        convertButton.setOnClickListener(newView.OnClickListener());{
-
-
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String metersStr = editTextMeters.getText().toString();
+                if (!metersStr.isEmpty()) {
+                    double meters = Double.parseDouble(metersStr);
+                    double inches = meters * 39.37;
+                    textviewResult.setText(String.format("%.2f meters is %.2f inches", meters, inches));
+                } else {
+                    textviewResult.setText("Please enter a value in meters");
+                }
             }
-        }
-
+        });
     }
 }
